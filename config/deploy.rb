eabb2052be2f650e1173e83b05565c2d2a010a7d
Kelly Mahan <kelly@digimedia.com>
2010-10-19 16:43:00 -0500

    changes to deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,22 +1,31 @@
 set :application, "capistrano_example"
-set :repository,  "set your repository location here"
+set :user, 'kmahan'
+set :ssh_options, { :forward_agent => true }
+set :deploy_to, "/home/#{user}/apps/#{application}"
 
-set :scm, :subversion
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
 
-# If you are using Passenger mod_rails uncomment this:
-# if you're still using the script/reapear helper you will need
-# these http://github.com/rails/irs_process_scripts
+set :scm, :git
+set :branch, "master"
+set :repository,  "git://github.com/KellyMahan/capistrano_example.git"
 
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+
+role :app, "192.168.1.54" # This may be the same as your `Web` server
+#role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
+#role :db,  "your slave db-server here"
+
+
+set :deploy_via, :remote_cache
+set :keep_releases, 10
+
+
+default_run_options[:pty] = true
+
+
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
